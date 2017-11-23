27648672575f87d111d0448a10173c8c6cc7d228
Jono King <jon_ok@hotmail.co.uk>
2017-11-23 15:50:19 +1300

    Updates app ID and password

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "c0998142-bbc1-431d-8fa7-09693362a518",
+    appPassword: "dhbxCG737dfcMXHLG70|##+"
 });
 
 // Listen for messages from users 
